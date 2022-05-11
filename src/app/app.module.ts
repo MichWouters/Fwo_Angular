6b7c56643b9dd54f3ab3368bc7d8c47b3f61a218
Michiel Wouters <michiel_woute64@hotmail.com>
2022-05-11 10:33:52 +0200

    Finish assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { AssignmentOneComponent } from './assignment-one/assignment-one.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { AssignmentOneComponent } from './assignment-one/assignment-one.componen
     ServerComponent,
     ServersComponent,
     AssignmentOneComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
   ],
   imports: [
     BrowserModule
