baf45fa64f4c6a5449dd88a4e1f47b6512ca658a
Michiel Wouters <michiel_woute64@hotmail.com>
2022-05-11 10:22:18 +0200

    Start Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AssignmentOneComponent } from './assignment-one/assignment-one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    ServersComponent,
+    AssignmentOneComponent,
   ],
   imports: [
     BrowserModule
