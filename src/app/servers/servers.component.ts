4b109929fb51f00ddcba9c6ebc73f36adf9fccec
Michiel Wouters <michiel_woute64@hotmail.com>
2022-05-11 11:40:59 +0200

    Discussed 1 way , property and method binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = "No server was created";
+  serverName: string = "TestServer";
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      "Server was created with name: " + this.serverName;
+
+  }
 
   ngOnInit(): void {
   }
