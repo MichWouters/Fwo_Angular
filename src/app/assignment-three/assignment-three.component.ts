0a3c34b1bf6c44063d660132c26d6ef70c41ab8c
Michiel Wouters <michiel_woute64@hotmail.com>
2022-05-25 12:31:50 +0200

    End of Assignment 3

--- src/app/assignment-three/assignment-three.component.ts
+++ src/app/assignment-three/assignment-three.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Holiday } from './holiday.model';
 
 @Component({
   selector: 'app-assignment-three',
@@ -6,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-three.component.css']
 })
 export class AssignmentThreeComponent implements OnInit {
+  public showSecret: boolean = false;
+  logs: string[] = [];
+  holidays: Holiday[] = [];
 
-  constructor() { }
+  constructor() {
+    this.generateHolidays();
+
+    setInterval(() => {
+      this.generateHolidays();
+    }, 5000);
+  }
+
+  generateHolidays(): void {
+  this.holidays = [
+      this.generateHoliday('Milan'),
+      this.generateHoliday('Paris'),
+      this.generateHoliday('Brussels'),
+      this.generateHoliday('Amsterdam'),
+      this.generateHoliday('Milan'),
+    ]
+  }
+
+  generateHoliday(name: string): Holiday {
+    return new Holiday(
+      name, Math.floor(Math.random() * 25) + 0)
+  }
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.logs.push('Hello');
+  }
 
   ngOnInit(): void {
   }
