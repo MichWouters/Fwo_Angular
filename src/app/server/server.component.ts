b354c929f1e03bdb75f9bb7aaf831e5f2334bbca
Michiel Wouters <michiel_woute64@hotmail.com>
2022-05-30 09:10:47 +0200

    Added Input variables on server.ts

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from "@angular/core";
     styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
-    serverId: number = 10;
-    serverName: string = 'server';
+    @Input() serverId: number = 10;
+    @Input() serverName: string = 'server';
     serverStatus: string = 'Offline';
 
     constructor() {
