137691a7734697049ad5506cd33843a8d73b3319
Michiel Wouters <michiel_woute64@hotmail.com>
2022-06-15 00:36:43 +0200

    End assingment 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { AssignmentOneComponent } from './assignments/assignment-one/assignment-one.component';
+
 import { SuccessAlertComponent } from './assignments/assignment-one/success-alert/success-alert.component';
 import { WarningAlertComponent } from './assignments/assignment-one/warning-alert/warning-alert.component';
-import { AssignmentTwoComponent } from './assignments/assignment-two/assignment-two.component';
-import { AssignmentThreeComponent } from './assignments/assignment-three/assignment-three.component';
 import { ColorsComponent } from './colors/colors.component';
 import { DirectiveExampleComponent } from './directives/directive-example/directive-example.component';
 
-
 import { BasicHighlightDirective } from './directives/basic-highlight.directive';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
-import { AssignmentFourComponent } from './assignments/assignment-four/assignment-four.component';
 import { AccountComponent } from './accounts/account/account.component';
 import { NewAccountComponent } from './accounts/new-account/new-account.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { LoggingService } from './services/logging.service';
-import { AssignmentFiveComponent } from './assignments/assignment-five/assignment-five.component';
 import { ActiveUsersComponent } from './assignments/assignment-five/active-users/active-users.component';
 import { InactiveUsersComponent } from './assignments/assignment-five/inactive-users/inactive-users.component';
 import { CounterService } from './services/counter.service';
-import { AssignmentSixComponent } from './assignments/assignment-six/assignment-six.component';
-import { AssignmentSixModule } from './assignments/assignment-six/assignment-six.module';
 import { AppRoutingModule } from './app-routing-module';
 import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
 import { TemplateFormComponent } from './forms/template-form/template-form.component';
+
+import { AssignmentOneComponent } from './assignments/assignment-one/assignment-one.component';
+import { AssignmentTwoComponent } from './assignments/assignment-two/assignment-two.component';
+import { AssignmentThreeComponent } from './assignments/assignment-three/assignment-three.component';
+import { AssignmentFourComponent } from './assignments/assignment-four/assignment-four.component';
+import { AssignmentFiveComponent } from './assignments/assignment-five/assignment-five.component';
+import { AssignmentSixComponent } from './assignments/assignment-six/assignment-six.component';
+import { AssignmentSixModule } from './assignments/assignment-six/assignment-six.module';
+import { AssignmentSevenComponent } from './assignments/assignment-seven/assignment-seven.component';
 
 @NgModule({
   declarations: [
@@ -37,22 +39,24 @@ import { TemplateFormComponent } from './forms/template-form/template-form.compo
     ServerComponent,
     ServersComponent,
     AssignmentOneComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
     AssignmentTwoComponent,
     AssignmentThreeComponent,
+    AssignmentFourComponent,
+    AssignmentFiveComponent,
+    AssignmentSixComponent,
+    AssignmentSevenComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+
     ColorsComponent,
     DirectiveExampleComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
-    AssignmentFourComponent,
     AccountComponent,
     NewAccountComponent,
     AccountsComponent,
-    AssignmentFiveComponent,
     ActiveUsersComponent,
     InactiveUsersComponent,
-    AssignmentSixComponent,
     ReactiveFormComponent,
     TemplateFormComponent,
   ],
