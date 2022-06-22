48d0e99ab4c799f2ca14bb1eb4177ea51af3fd8c
Michiel Wouters <michiel_woute64@hotmail.com>
2022-06-22 09:24:05 +0200

    Added Assignment 7 solution

--- src/app/assignments/assignment-seven/assignment-seven.component.ts
+++ src/app/assignments/assignment-seven/assignment-seven.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyCustomValidator } from './custom-validator';
 
 @Component({
   selector: 'app-assignment-seven',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-seven.component.css']
 })
 export class AssignmentSevenComponent implements OnInit {
-
+  projectForm!: FormGroup
   constructor() { }
 
   ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, MyCustomValidator.invalidProjectName]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('stable'),
+    });
+  }
+
+  onSaveProject():void{
+    console.log(this.projectForm);
   }
 
 }
